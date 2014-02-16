56d8858a8a47915292609dfd0af785599db7a055
Itsik <extropia@gmail.com>
2014-02-16 11:35:41 +0200

    multiSelectMenu directive

---
+++ test/spec/filters/as.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Filter: as', function () {
+
+  // load the filter's module
+  beforeEach(module('gsUiInfraApp'));
+
+  // initialize a new instance of the filter before each test
+  var as;
+  beforeEach(inject(function ($filter) {
+    as = $filter('as');
+  }));
+
+  it('should return the input prefixed with "as filter:"', function () {
+    var text = 'angularjs';
+    expect(as(text)).toBe('as filter: ' + text);
+  });
+
+});
