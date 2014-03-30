2f755dd0e451901dbde431d5994bbf98a6ff1900
itsikm <itsik@gigspaces.com>
2014-03-30 16:46:05 +0300

    blueprint arrows bug fix

--- app/scripts/directives/blueprintArrows.js
+++ app/scripts/directives/blueprintArrows.js
@@ -45,15 +45,16 @@ angular.module('gsUiInfraApp')
                         .enter()
                         .append('svg:marker')
                         .attr('id', 'arrowhead')
-                        .attr('viewBox', '0 -5 10 10')
-                        .attr('refX', 9)
-                        .attr('refY', 0)
-                        .attr('markerWidth', 4)
-                        .attr('markerHeight', 5)
-                        .attr('orient', 'auto')
+                        .attr("viewBox", "0 -5 10 10")
+                        .attr("refX", 9)
+                        .attr("refY", 0)
+                        .attr("markerUnits", "strokeWidth")
+                        .attr("markerWidth", 6)
+                        .attr("markerHeight", 6)
+                        .attr("orient", "auto")
                         .attr('fill', lineColor)
-                        .append('svg:path')
-                        .attr('d', 'M0,-5L10,0L0,5');
+                        .append("path")
+                        .attr("d", "M0,-5L10,0L0,5");
                 }
 
                 $scope.$watch('coordinates', function(data){
@@ -197,7 +198,10 @@ angular.module('gsUiInfraApp')
                     to = relation.target;
 
                 if(data[from] != undefined && data[to] != undefined) {
-                    Coords.push(getNearestPoints(from, to));
+                    var thisCord = getNearestPoints(from, to);
+                    if(thisCord !== null) {
+                        Coords.push(thisCord);
+                    }
                 }
             });
             angular.extend(coordinates, Coords);
@@ -215,19 +219,24 @@ angular.module('gsUiInfraApp')
                 collector,
                 current;
 
-            angular.forEach(fromPoint, function (f, fi) {
-                angular.forEach(toPoint, function (t, ti) {
-                    current = Math.max(Math.abs(f.x - t.x), Math.abs(f.y - t.y)); // Calculate distance between points
-                    if (!nearest || current < nearest) {
-                        nearest = current;  // update the lower value of distance between points
-                        collector = { // Set the index of the nearest point until now
-                            "from": fi,
-                            "to": ti
-                        };
-                    }
+            if(fromPoint !== null && toPoint !== null) {
+                angular.forEach(fromPoint, function (f, fi) {
+                    angular.forEach(toPoint, function (t, ti) {
+                        current = Math.max(Math.abs(f.x - t.x), Math.abs(f.y - t.y)); // Calculate distance between points
+                        if (!nearest || current < nearest) {
+                            nearest = current;  // update the lower value of distance between points
+                            collector = { // Set the index of the nearest point until now
+                                "from": fi,
+                                "to": ti
+                            };
+                        }
+                    });
                 });
-            });
-            return [fromPoint[collector.from], toPoint[collector.to]];
+                return [fromPoint[collector.from], toPoint[collector.to]];
+            }
+            else {
+                return null;
+            }
         }
 
         /****************
@@ -237,6 +246,10 @@ angular.module('gsUiInfraApp')
          */
         function getAttachPointes( id )
         {
+            if(!elements.hasOwnProperty(id)) {
+                return null;
+            }
+
             var callback = [],
                 width  = elements[id].outerWidth(),
                 height = elements[id].outerHeight(),
