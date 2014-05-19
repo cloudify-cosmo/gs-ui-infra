13a3fcd0c56db0b81ac68ec5236d51d40b06fe08
itsikm <itsik@gigspaces.com>
2014-05-19 13:50:57 +0300

    Debug

--- app/scripts/directives/pieProgress.js
+++ app/scripts/directives/pieProgress.js
@@ -1,103 +1,103 @@
-//'use strict';
-//
-//angular.module('gsUiInfraApp')
-//    .directive('pieProgress', function () {
-//        return {
-//            restrict: 'A',
-//            scope: {
-//                'data': '=pieProgress'
-//            },
-//            link: function postLink($scope, $element, $attrs) {
-//
-//                var defaultSize = 200,
-//                    ratio = 3.5,
-//                    width = $attrs.size || defaultSize,
-//                    height = $attrs.size || defaultSize,
-//                    innerSize = width - (width / ratio),
-//                    pi = Math.PI,
-//                    towPi = pi * 2;
-//
-//                // Collection of colors
-//                var color = d3.scale.category20();
-//
-//                // listener
-//                $scope.$watch('data', function (data, previous) {
-//                    data = d3.entries(data);
-//                    previous = d3.entries(previous);
-//                    layout(data, previous);
-//                    render(data);
-//                }, true);
-//
-//                // define angles of arcs
-//                function layout(data, previous) {
-//                    var startAngle = 0;
-//                    for (var i = 0; i < data.length; i++) {
-//                        var arc = data[i];
-//                        if (previous.hasOwnProperty(i)) {
-//                            var oldArc = previous[i];
-//                            arc.previous = startAngle + convertToRadian(oldArc.value);
-//                        }
-//                        else {
-//                            arc.previous = startAngle;
-//                        }
-//                        arc.startAngle = startAngle;
-//                        arc.endAngle = startAngle = startAngle + convertToRadian(arc.value);
-//                    }
-//                }
-//
-//                function convertToRadian(data) {
-//                    return data / 100 * towPi;
-//                }
-//
-//                function render(data) {
-//
-//                    // Select groups and bind data
-//                    var g = vis.selectAll('g')
-//                        .data(data);
-//
-//                    // arc configuration
-//                    function configArc( group ) {
-//                        return group.attr('fill', function (d, i) { return color(i); })
-//                            .attr('class', function(d){ return d.key; })
-//                            .transition()
-//                            .ease('bounce')
-//                            .duration(750)
-//                            .attrTween('d', arcTween);
-//                    }
-//
-//                    // update existing data
-//                    configArc(g.select('path'));
-//
-//                    // enter for new data
-//                    configArc(g.enter()
-//                        .append('g')
-//                        .append('svg:path')
-//                    );
-//
-//                    // remove old data
-//                    configArc(g.exit()).remove();
-//                }
-//
-//                // interpolate transition
-//                function arcTween(d) {
-//                    var interpolate = d3.interpolate(d.previous, d.endAngle);
-//                    return function (t) {
-//                        d.endAngle = interpolate(t);
-//                        return arc(d);
-//                    };
-//                }
-//
-//                // Create SVG layer
-//                var vis = d3.select($element[0]).append('svg')
-//                    .attr('width', width)
-//                    .attr('height', height)
-//                    .append('g')
-//                    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-//
-//                // Define Arc
-//                var arc = d3.svg.arc()
-//                    .innerRadius(innerSize / 2)
-//                    .outerRadius(width / 2);
-//            }
-//        };
-//    });
+'use strict';
+
+angular.module('gsUiInfraApp')
+    .directive('pieProgress', function () {
+        return {
+            restrict: 'A',
+            scope: {
+                'data': '=pieProgress'
+            },
+            link: function postLink($scope, $element, $attrs) {
+
+                var defaultSize = 200,
+                    ratio = 3.5,
+                    width = $attrs.size || defaultSize,
+                    height = $attrs.size || defaultSize,
+                    innerSize = width - (width / ratio),
+                    pi = Math.PI,
+                    towPi = pi * 2;
+
+                // Collection of colors
+                var color = d3.scale.category20();
+
+                // listener
+                $scope.$watch('data', function (data, previous) {
+                    data = d3.entries(data);
+                    previous = d3.entries(previous);
+                    layout(data, previous);
+                    render(data);
+                }, true);
+
+                // define angles of arcs
+                function layout(data, previous) {
+                    var startAngle = 0;
+                    for (var i = 0; i < data.length; i++) {
+                        var arc = data[i];
+                        if (previous.hasOwnProperty(i)) {
+                            var oldArc = previous[i];
+                            arc.previous = startAngle + convertToRadian(oldArc.value);
+                        }
+                        else {
+                            arc.previous = startAngle;
+                        }
+                        arc.startAngle = startAngle;
+                        arc.endAngle = startAngle = startAngle + convertToRadian(arc.value);
+                    }
+                }
+
+                function convertToRadian(data) {
+                    return data / 100 * towPi;
+                }
+
+                function render(data) {
+
+                    // Select groups and bind data
+                    var g = vis.selectAll('g')
+                        .data(data);
+
+                    // arc configuration
+                    function configArc( group ) {
+                        return group.attr('fill', function (d, i) { return color(i); })
+                            .attr('class', function(d){ return d.key; })
+                            .transition()
+                            .ease('bounce')
+                            .duration(750)
+                            .attrTween('d', arcTween);
+                    }
+
+                    // update existing data
+                    configArc(g.select('path'));
+
+                    // enter for new data
+                    configArc(g.enter()
+                        .append('g')
+                        .append('svg:path')
+                    );
+
+                    // remove old data
+                    configArc(g.exit()).remove();
+                }
+
+                // interpolate transition
+                function arcTween(d) {
+                    var interpolate = d3.interpolate(d.previous, d.endAngle);
+                    return function (t) {
+                        d.endAngle = interpolate(t);
+                        return arc(d);
+                    };
+                }
+
+                // Create SVG layer
+                var vis = d3.select($element[0]).append('svg')
+                    .attr('width', width)
+                    .attr('height', height)
+                    .append('g')
+                    .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+                // Define Arc
+                var arc = d3.svg.arc()
+                    .innerRadius(innerSize / 2)
+                    .outerRadius(width / 2);
+            }
+        };
+    });
--- app/scripts/services/utils.js
+++ app/scripts/services/utils.js
@@ -1,139 +1,139 @@
-//'use strict';
-//
-//angular.module('gsUiInfraApp')
-//    .factory('Utils', function Utils($log) {
-//
-//        var depth = -1;
-//
-//        return {
-//
-//            /**
-//             * Performs merging of an array into another, provided that the target array
-//             * is larger or equal in size. Items in the target array will be replaced with items from the
-//             * source array, starting index 0 (on both), and retaining the original order.
-//             *
-//             * @param source The array from which to fuse.
-//             * @param target The array to fuse into.
-//             * @returns {Array} The result of the fuse, or the target array if source is falsy,
-//             *          or if the source is larger than the target.
-//             */
-//            dissolveArray: function (source, target) {
-//                var _target = target;
-//                if (source && source.length && target && target.length && source.length <= target.length) {
-//                    var _source = source;
-//                    _target.splice(0, _source.length);
-//                    _target = _source.concat(_target);
-//                }
-//                return _target;
-//            },
-//
-//            /**
-//             * Returns a new array with items matching the property name and value of
-//             * the passed array.
-//             *
-//             * @param arr The array to filter.
-//             * @param propName The property name to filter by.
-//             * @param propValue The property value to filter by.
-//             * @returns {Array} A new filtered array.
-//             */
-//            filter: function (arr, propName, propValue) {
-//                try {
-//                    var filtered = [];
-//                    var i = arr.length;
-//                    while (i--) {
-//                        var item = arr[i];
-//                        if (item.hasOwnProperty(propName) && this.equals(item[propName], propValue)) {
-//                            filtered.push(item);
-//                        }
-//                    }
-//                    return filtered;
-//                } catch (e) {
-//                    $log.info([arr, propName, propValue]);
-//                    throw e;
-//                }
-//            },
-//
-//            /**
-//             * Used for comparisons, including array comparisons (only for sorted arrays).
-//             */
-//            equals: function (a, b) {
-//                if (a !== b) {
-//                    return false;
-//                }
-//                if (a === null || b === null) {
-//                    return false;
-//                }
-//                if (a.length !== b.length) {
-//                    return false;
-//                }
-//                // handles sorted arrays only
-//                for (var i = 0; i < a.length; ++i) {
-//                    if (a[i] !== b[i]) {
-//                        return false;
-//                    }
-//                }
-//                return true;
-//            },
-//
-//            /**
-//             * Finds the first match of an item according the a property name
-//             * and value in the passed array.
-//             *
-//             * @param arr The array to search.
-//             * @param propName The property name to search by.
-//             * @param propValue The property value to search by.
-//             * @returns {*|boolean} The matched item, or false, if no such item
-//             * was found.
-//             */
-//            findBy: function (arr, propName, propValue) {
-//                var i = arr.length;
-//                while (i--) {
-//                    var item = arr[i];
-//                    if (item.hasOwnProperty(propName) && item[propName] === propValue) {
-//                        return item;
-//                    }
-//                }
-//                return false;
-//            },
-//
-//            /**
-//             * Maps the property values of an object and returns an array.
-//             *
-//             * @param obj The object to map.
-//             * @returns {*} An array with the object's property values.
-//             */
-//            propValues: function (obj) {
-//                return Object.keys(obj).map(function (key) {
-//                    return obj[key];
-//                });
-//            },
-//
-//            /**
-//             * Traverse a tree (breadth first) and act on each node.
-//             *
-//             * @param node A root node to traverse from.
-//             * @param sorter (Optional) A function for Array.sort to sort each node's children.
-//             * @param downHandler (Optional) A handler function to act on each node, when traversing down the tree.
-//             * @param upHandler (Optional) A handler function to act on each node, when traversing up the tree.
-//             * @returns {*}
-//             */
-//            walk: function (node, sorter, downHandler, upHandler) {
-//                depth++;
-//                // sort children
-//                sorter && node.children.sort(sorter);
-//                var i = node.children.length;
-//                while (i--) {
-//                    var child = node.children[i];
-//                    // act on each child node, traversing down
-//                    downHandler && downHandler(child, node, i, depth);
-//                    // continue with traversal
-//                    child.children && child.children.length && this.walk(child, sorter, downHandler, upHandler);
-//                    // act on each parent node, traversing up
-//                    upHandler && upHandler(child, node, i, depth);
-//                }
-//                depth--;
-//                return node;
-//            }
-//
-//        };
-//    });
+'use strict';
+
+angular.module('gsUiInfraApp')
+    .factory('Utils', function Utils($log) {
+
+        var depth = -1;
+
+        return {
+
+            /**
+             * Performs merging of an array into another, provided that the target array
+             * is larger or equal in size. Items in the target array will be replaced with items from the
+             * source array, starting index 0 (on both), and retaining the original order.
+             *
+             * @param source The array from which to fuse.
+             * @param target The array to fuse into.
+             * @returns {Array} The result of the fuse, or the target array if source is falsy,
+             *          or if the source is larger than the target.
+             */
+            dissolveArray: function (source, target) {
+                var _target = target;
+                if (source && source.length && target && target.length && source.length <= target.length) {
+                    var _source = source;
+                    _target.splice(0, _source.length);
+                    _target = _source.concat(_target);
+                }
+                return _target;
+            },
+
+            /**
+             * Returns a new array with items matching the property name and value of
+             * the passed array.
+             *
+             * @param arr The array to filter.
+             * @param propName The property name to filter by.
+             * @param propValue The property value to filter by.
+             * @returns {Array} A new filtered array.
+             */
+            filter: function (arr, propName, propValue) {
+                try {
+                    var filtered = [];
+                    var i = arr.length;
+                    while (i--) {
+                        var item = arr[i];
+                        if (item.hasOwnProperty(propName) && this.equals(item[propName], propValue)) {
+                            filtered.push(item);
+                        }
+                    }
+                    return filtered;
+                } catch (e) {
+                    $log.info([arr, propName, propValue]);
+                    throw e;
+                }
+            },
+
+            /**
+             * Used for comparisons, including array comparisons (only for sorted arrays).
+             */
+            equals: function (a, b) {
+                if (a !== b) {
+                    return false;
+                }
+                if (a === null || b === null) {
+                    return false;
+                }
+                if (a.length !== b.length) {
+                    return false;
+                }
+                // handles sorted arrays only
+                for (var i = 0; i < a.length; ++i) {
+                    if (a[i] !== b[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            },
+
+            /**
+             * Finds the first match of an item according the a property name
+             * and value in the passed array.
+             *
+             * @param arr The array to search.
+             * @param propName The property name to search by.
+             * @param propValue The property value to search by.
+             * @returns {*|boolean} The matched item, or false, if no such item
+             * was found.
+             */
+            findBy: function (arr, propName, propValue) {
+                var i = arr.length;
+                while (i--) {
+                    var item = arr[i];
+                    if (item.hasOwnProperty(propName) && item[propName] === propValue) {
+                        return item;
+                    }
+                }
+                return false;
+            },
+
+            /**
+             * Maps the property values of an object and returns an array.
+             *
+             * @param obj The object to map.
+             * @returns {*} An array with the object's property values.
+             */
+            propValues: function (obj) {
+                return Object.keys(obj).map(function (key) {
+                    return obj[key];
+                });
+            },
+
+            /**
+             * Traverse a tree (breadth first) and act on each node.
+             *
+             * @param node A root node to traverse from.
+             * @param sorter (Optional) A function for Array.sort to sort each node's children.
+             * @param downHandler (Optional) A handler function to act on each node, when traversing down the tree.
+             * @param upHandler (Optional) A handler function to act on each node, when traversing up the tree.
+             * @returns {*}
+             */
+            walk: function (node, sorter, downHandler, upHandler) {
+                depth++;
+                // sort children
+                sorter && node.children.sort(sorter);
+                var i = node.children.length;
+                while (i--) {
+                    var child = node.children[i];
+                    // act on each child node, traversing down
+                    downHandler && downHandler(child, node, i, depth);
+                    // continue with traversal
+                    child.children && child.children.length && this.walk(child, sorter, downHandler, upHandler);
+                    // act on each parent node, traversing up
+                    upHandler && upHandler(child, node, i, depth);
+                }
+                depth--;
+                return node;
+            }
+
+        };
+    });
