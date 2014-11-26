462a986cf9982cc458bcfb62889082741ba8bcdb
Itsik <extropia@gmail.com>
2014-11-26 12:52:43 +0200

    CFY-1582 - Add unit-test

--- test/spec/directives/multiSelectMenu.js
+++ test/spec/directives/multiSelectMenu.js
@@ -2,15 +2,52 @@
 
 describe('Directive: multiSelectMenu', function () {
 
-/*
-  beforeEach(module('gsUiInfraApp'));
+    var element, scope;
 
-  var element;
+    beforeEach(module('gsUiInfraApp'));
 
-  it('should make hidden element visible', inject(function ($rootScope, $compile) {
-    element = angular.element('<multi-select-menu></multi-select-menu>');
-    element = $compile(element)($rootScope);
-    expect(element.text()).toBe('this is the multiSelectMenu directive');
-  }));
-*/
+    describe('Test setup', function() {
+        beforeEach(inject(function($rootScope, $compile, $document){
+            element = angular.element('' +
+                '<div multi-select-menu ' +
+                'data-multiple="true" ' +
+                'data-options="options" ' +
+                'data-ng-model="selected" ' +
+                'data-init="TestValue" ' +
+                'data-text="Choose Options" ' +
+                'data-listener="true" ' +
+                'name="blueprints"></div>');
+
+            scope = $rootScope;
+
+            scope.options = [
+                {'label': 'TestLabel', 'value': 'TestValue'}
+            ];
+
+            $document.click = function() {};
+            $document.keyup = function() {};
+
+            $compile(element)(scope);
+            scope.$digest();
+        }));
+
+        it ('should have initialize default value', function(){
+            expect(scope.selected[0].value).toBe('TestValue');
+        });
+
+        it ('should deselect non-relevant selected options', function(){
+            scope.options = [
+                {'label': 'NewLabel', 'value': 'NewValue'}
+            ];
+            scope.$apply();
+
+            waitsFor(function(){
+                return scope.selected.length === 0;
+            }, 'cleaning the non-relevant selected options options', 5000)
+
+            runs(function(){
+                expect(scope.selected.length).toBe(0);
+            });
+        });
+    });
 });
