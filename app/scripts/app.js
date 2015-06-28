583818a05dcc818d0c67775291bc4c6102dd5ed3
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-06-28 09:10:27 +0300

    make change backward compatible

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
+// make new templates module backward compatible. add it is not defined.
+try {
+    angular.module('gsUiInfraAppTemplates');
+} catch (err) {
+    /* failed to require */
+    angular.module('gsUiInfraAppTemplates',[]);
+}
+
 angular.module('gsUiInfraApp', ['gsUiInfraAppTemplates']);
